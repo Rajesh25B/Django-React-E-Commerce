f1fce295042e6d22b0928f9bbe6b01ae0f6f8a73
Rajesh25B <basvoju.rajesh@gmail.com>
2021-03-28 21:26:04 +0530

    Reactify the product's urls and navbar urls(cart&login) functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 // npm-bootstrap import
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // local imports
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div>
+    // wrap the entire component in a router
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
        </Container>
       </main>  
       <Footer />
       
-    </div>
+    </Router>
   );
 }
 
