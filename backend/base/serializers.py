aab8b29fa769be57ffbbbf37499a081e21f20c55
Rajesh25B <basvoju.rajesh@gmail.com>
2021-04-11 16:58:20 +0530

    implement UserSerializer and Protected Routes(permission_classes for admin user using RefreshToken)

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,8 +1,48 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from .models import Product
 
+
+class UserSerializer(serializers.ModelSerializer):
+    '''User serializer for User Model'''
+
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get_name(self, obj): # get the name of the user obj that is equal to first_name
+        name = obj.first_name
+        if name=='':
+            name = obj.email
+        return name
+    
+    def get__id(self, obj):
+        return obj.id
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    '''Product serializer for Product Model'''
     class Meta:
         model = Product
         fields = '__all__'
+
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,15 @@
 # django & DRF imports
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # local imports
 from .models import Product
+from django.contrib.auth.models import User
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
 
 # simpleJWT imports
@@ -16,13 +18,16 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     '''Serializer to obtain the username and email from the token'''
+    
     def validate(self, attrs):
         data = super().validate(attrs)
 
         refresh = self.get_token(self.user)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
@@ -51,10 +56,25 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
-def getProducts(request): # returns all products from products.py
+@permission_classes([IsAuthenticated])
+def getUserProfile(request): # returns the user
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request): # returns all users only for admin user
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProducts(request): # returns all products from Product model
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getProduct(request, pk): # returns single product from all products
