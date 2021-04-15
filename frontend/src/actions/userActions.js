2e01e987b2341418d312eee42fccc42703a96699
Rajesh25B <basvoju.rajesh@gmail.com>
2021-04-15 18:45:04 +0530

    implement Frontend User Registration functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,8 +2,13 @@ import axios from 'axios'
 import { 
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS, 
-    USER_LOGIN_FAIL, 
-    USER_LOGOUT } from '../constants/userConstants'
+    USER_LOGIN_FAIL,
+
+    USER_LOGOUT,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS, 
+    USER_REGISTER_FAIL, } from '../constants/userConstants'
 
 // login functionality
 export const login = (email, password) => async (dispatch) => {
@@ -23,12 +28,16 @@ export const login = (email, password) => async (dispatch) => {
             {'username':email, 'password':password},
             config
             )
+
             dispatch({
                 type:USER_LOGIN_SUCCESS,
                 payload:data
             })
+
             localStorage.setItem('userInfo', JSON.stringify(data))
+
     } catch (error) {
+        
         dispatch({
             type: USER_LOGIN_FAIL,
             payload: error.response && error.response.data.detail
@@ -43,4 +52,47 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type:USER_LOGOUT})
+}
+
+// Register functionality
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers:{
+                'Content-type':'application/json'
+            }
+        }
+        // api call to user login
+        const {data} = await axios.post(
+            '/api/users/register/',
+            {'name': name, 'email':email, 'password':password},
+            config
+            )
+            
+            dispatch({
+                type:USER_REGISTER_SUCCESS,
+                payload:data
+            })
+            // After user register success, dispatch as login success
+            dispatch({
+                type:USER_LOGIN_SUCCESS,
+                payload:data
+            })
+
+            localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+        })
+    }
+
 }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,8 +1,13 @@
 import { 
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS, 
-    USER_LOGIN_FAIL, 
-    USER_LOGOUT } from '../constants/userConstants'
+    USER_LOGIN_FAIL,
+
+    USER_LOGOUT,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL, } from '../constants/userConstants'
 
     export const userLoginReducer = (state = { }, action) => {
         switch(action.type) {
@@ -13,6 +18,25 @@ import {
                 return { loading:false, userInfo:action.payload }
     
             case USER_LOGIN_FAIL:
+                return { loading:false, error:action.payload }
+            
+            case USER_LOGOUT:
+                return {}
+            
+            default:
+                return state
+        }
+    }
+
+    export const userRegisterReducer = (state = { }, action) => {
+        switch(action.type) {
+            case USER_REGISTER_REQUEST:
+                return { loading:true }
+    
+            case USER_REGISTER_SUCCESS:
+                return { loading:false, userInfo:action.payload }
+    
+            case USER_REGISTER_FAIL:
                 return { loading:false, error:action.payload }
             
             case USER_LOGOUT:
