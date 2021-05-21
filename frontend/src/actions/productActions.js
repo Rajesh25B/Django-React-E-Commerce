b7a83929cacadcda6efb3c3a68496d704379e317
Rajesh25B <basvoju.rajesh@gmail.com>
2021-05-21 16:57:21 +0530

    implement product search box functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -27,11 +27,11 @@ import {
 
 // This function gonna be incharge of replacing API call that we made in HomeScreen.js
 
-export const listProducts = () => async(dispatch) => {
+export const listProducts = (keyword = '') => async(dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get('/api/products/')
+        const { data } = await axios.get(`/api/products${keyword}`)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
