2881547daf172a5adc193dc3dee0861aa8428237
Rajesh25B <basvoju.rajesh@gmail.com>
2021-05-24 18:13:42 +0530

    Add Hashrouting system

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,6 +1,6 @@
 // npm-bootstrap imports
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 // local imports
 import Header from './components/Header'
